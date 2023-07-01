2ad3b484f4ce4a8b1e2136a66306d5865bed4e67
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2023-07-02 03:53:40 +0800

    updating the files

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -8,7 +8,7 @@ const LoginPage: React.FC = () => {
 
   const handleLogin = async (email: string, password: string) => {
     try {
-      const response = await fetch('https://hris-armada.vercel.app/sign-in', {
+      const response = await fetch('http://hris-armada.vercel.app/sign-in', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
