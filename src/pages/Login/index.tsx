7660178548255f1837e7815525f1f004ee2529d5
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2023-07-07 07:12:39 +0800

    enable /dashboard

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -8,7 +8,7 @@ const LoginPage: React.FC = () => {
 
   const handleLogin = async (email: string, password: string) => {
     try {
-      const response = await fetch('https://hrsystem-back.empireonecontactcenter.com/sign-in', {
+      const response = await fetch('http://localhost:3000/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import PublicRoute from './public-route';
 import { Page404Error } from '../pages/404Error/index';
-import LoginPage from '../pages/Login';
+// import LoginPage from '../pages/Login';
 import SignUpPage from '../pages/SignUp';
 import DashboardPage from '../pages/dashboard'
 import RecruitmentPage from '../pages/recruitment';
@@ -12,9 +12,9 @@ const Routes: React.FC = () => {
 
   return (
     <Switch>
-      <PublicRoute exact path="/" component={LoginPage} />
+      {/* <PublicRoute exact path="/" component={LoginPage} />
       <PublicRoute exact path="/sign-in" component={LoginPage} />
-      <PublicRoute exact path="/index" component={LoginPage} />
+      <PublicRoute exact path="/index" component={LoginPage} /> */}
       <PublicRoute exact path="/sign-up" component={SignUpPage} />
       <PublicRoute exact path="/dashboard" component={DashboardPage} />
       <PublicRoute exact path="/recruitment" component={RecruitmentPage} />
