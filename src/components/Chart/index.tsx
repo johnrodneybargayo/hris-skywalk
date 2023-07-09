8e8101b76b1b5df58695a71383dbfc52cbc6f4f1
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2023-07-10 03:15:19 +0800

    updating the Job Statistics

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import './styles.scss';
 
+
 const Chart = () => {
   return (
     <div className="chart-container">
-    <div className="igFrameBar">
-      <div className="igData igData1"></div>
-      <div className="igData igData2"></div>
-      <div className="igData igData3"></div>
-      <div className="igData igData4"></div>
-      <div className="igData igData5"></div>
-      <div className="igData igData6"></div>
-      <div className="igData igData7"></div>
-      <div className="igData igData8"></div>
-      <div className="igData igData9"></div>
-      <div className="igData igData10"></div> 
+      <div className="igFrameBar">
+        <div className="igData igData1"></div>
+        <div className="igData igData2"></div>
+        <div className="igData igData3"></div>
+        <div className="igData igData4"></div>
+        <div className="igData igData5"></div>
+        <div className="igData igData6"></div>
+        <div className="igData igData7"></div>
+      </div>
+      <div className='month'>
+        <button className='month-button'>Month</button>
+      </div>
+      <div className='week'>
+        <button className='week-button'>Week</button>
+      </div>
+      <div className='day'>
     </div>
-    </div>
+    <div className='Job'>
+      <p>Job Applied</p>
+      <p>Job</p>
+      </div>
+</div>
   );
 };
 
