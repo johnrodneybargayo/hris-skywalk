a45e89affba0917b84bcb8e1ae3180d91f5a8057
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2023-08-19 05:32:26 +0800

    adding cookies to the code and also authentication requirement

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import axios from 'axios'; // Import Axios
+import axios from 'axios';
 import LoginForm from '../../components/forms/Login';
 
 const LoginPage: React.FC = () => {
@@ -9,27 +9,25 @@ const LoginPage: React.FC = () => {
 
   const handleLogin = async (email: string, password: string) => {
     try {
-      const token = localStorage.getItem('accessToken');
       const apiKey = 'AIzaSyAI1NsFZrRaBSRCtj8TkIxA3Mg-qYFDRzg'; // Replace with your actual API key
 
-      const response = await axios.post('https://empireone-global-inc.uc.r.appspot.com/api/login', {
-        //  const response = await axios.post('http://localhost:8080/api/login', {
-        email,
-        password,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-          'X-API-Key': apiKey,
+      const response = await axios.post(
+        'http://localhost:8080/api/login',
+        {
+          email,
+          password,
         },
-      });
-
-      console.log('Response:', response); // Add this logging statement
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': apiKey,
+          },
+        }
+      );
 
       if (response.status === 200) {
         const data = response.data;
         if (data.token) {
-          console.log('Login successful');
           localStorage.setItem('accessToken', data.token);
           history.push('/dashboard');
         } else {
