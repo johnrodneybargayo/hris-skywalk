3e754a8cb4de3f56e7e12cddb0e2e25ea9d39517
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2023-08-22 09:31:48 +0800

    fixed userDetails image display

--- src/components/modals/userDetails/userDetail.tsx
+++ src/components/modals/userDetails/userDetail.tsx
@@ -125,7 +125,7 @@ const UserDetailModal: React.FC<UserDetailProps> = ({ showModal, onClose, formDa
 
         // Create a new user object with relevant data
         const newUser = {
-          image: applicantData.image,
+          Image: applicantData.image,
           email: applicantData.email,
           password: '', // Temporary password, consider generating a secure one
           firstName: applicantData.firstName,
@@ -223,7 +223,6 @@ const UserDetailModal: React.FC<UserDetailProps> = ({ showModal, onClose, formDa
             <div className='user-details-images'>
             <img src={applicantData.image} alt="Applicant's E-Signature" />
             </div>
-            <div className='user-details-image-note'>Upload your photo here, make sure it's a decent photo for your employee ID</div>
             <div className='user-details-esignature'>
               {/* Code for e-signature images */}
             </div>
