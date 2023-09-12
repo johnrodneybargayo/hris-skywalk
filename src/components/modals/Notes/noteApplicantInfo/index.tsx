2e9599474fdf19988b93e32f2a14978de91b3ad5
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2023-09-13 05:01:32 +0800

    add note and display notes

--- src/components/modals/Notes/noteApplicantInfo/index.tsx
+++ src/components/modals/Notes/noteApplicantInfo/index.tsx
@@ -34,35 +34,35 @@ const NotesModal: React.FC<NotesModalProps> = ({
   const [selectedStatus, setSelectedStatus] = useState('');
   const [userData, setUserData] = useState<UserData | null>(null);
 
-const fetchUserData = useCallback(async () => {
-  try {
-    const accessToken = Cookies.get('accessToken');
+  const fetchUserData = useCallback(async () => {
+    try {
+      const accessToken = Cookies.get('accessToken');
 
-    if (!accessToken) {
-      console.error('User is not authenticated');
-      return;
+      if (!accessToken) {
+        console.error('User is not authenticated');
+        return;
+      }
+
+      console.log('Attempting to fetch user data...');
+      const response = await axios.get<UserData>('https://empireone-global-inc.uc.r.appspot.com/api/users/profile', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      console.log('Response received:', response); // Log the entire response object
+
+      if (response.status === 200) {
+        console.log('User Data:', response.data);
+        setUserData(response.data); // Use setUserData to update userData
+      } else {
+        console.error('Unexpected response:', response);
+      }
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      // Handle the error, e.g., display an error message to the user
     }
-
-    console.log('Attempting to fetch user data...');
-    const response = await axios.get<UserData>('https://empireone-global-inc.uc.r.appspot.com/api/users/profile', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    console.log('Response received:', response); // Log the entire response object
-
-    if (response.status === 200) {
-      console.log('User Data:', response.data);
-      setUserData(response.data); // Use setUserData to update userData
-    } else {
-      console.error('Unexpected response:', response);
-    }
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    // Handle the error, e.g., display an error message to the user
-  }
-}, []); // Make sure to update the state variable used here, e.g., setUserData
+  }, []); // Make sure to update the state variable used here, e.g., setUserData
 
 
   const fetchNotes = useCallback(async () => {
@@ -167,13 +167,22 @@ const fetchUserData = useCallback(async () => {
       <div className={styles['modal-overlay']} onClick={handleModalClose}></div>
       <div className={styles['modal-content-notes']}>
         <div className={styles['close-icon-notes']} onClick={handleModalClose}>
-          {/* Close icon SVG */}
+          <svg height="25px" width="25px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <path style={{ fill: '#3a3672' }} d="M0,256c0,141.384,114.615,256,256,256l22.261-256L256,0C114.615,0,0,114.615,0,256z"></path>
+              <path style={{ fill: '#1C274C' }} d="M256,0v512c141.384,0,256-114.616,256-256S397.384,0,256,0z"></path>
+              <polygon style={{ fill: '#FFFFFF' }} points="161.555,114.333 114.333,161.555 208.778,256 114.333,350.445 161.555,397.667 256,303.222 278.261,256 256,208.778 "></polygon>
+              <polygon style={{ fill: '#cfc2ff' }} points="397.667,161.555 350.445,114.333 256,208.778 256,303.222 350.445,397.667 397.667,350.445 303.222,256 "></polygon>
+            </g>
+          </svg>
         </div>
         <h2 className={styles['notes-label']}>Add Notes</h2>
         {userData && (
           <div className={styles['user-details']}>
             <span className={styles['user-name']}>
-             Interviewer: {userData.firstName} {userData.lastName}
+              Interviewer: {userData.firstName} {userData.lastName}
             </span>
             {/* Display other user profile fields here */}
           </div>
